ed7c0b92951d95937cebd7e5a067fa8284a92839
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-26 14:13:24 -0700

    add data and images

---
+++ src/books.js
@@ -0,0 +1,59 @@
+const books = [
+  {
+    name: 'Funny Weather',
+    author: 'Olivia Laing',
+    genre: 'Art Criticism',
+    year: '2020',
+    image: 'books/funnyweather.jpg',
+  },
+  {
+    name: 'The World of Madelon Vriesendorp',
+    author: 'Shuman Basar and Stephan Trüby',
+    genre: 'Architecture',
+    year: '2008',
+    image: 'books/madelon.jpg',
+  },
+  {
+    name: 'Delirious New York',
+    author: 'Rem Koolhaas',
+    genre: 'Architecture',
+    year: '1978',
+    image: 'books/newyork.jpeg',
+  },
+  {
+    name: 'How to Make a Japanese House',
+    author: 'Cathelijne Nuijsink',
+    genre: 'Architecture',
+    year: '2012',
+    image: 'books/house.jpg',
+  },
+  {
+    name: 'Handbook in Motion',
+    author: 'Simone Forti',
+    genre: 'Dance',
+    year: '1974',
+    image: 'books/handbook.jpg',
+  },
+  {
+    name: 'There You Are',
+    author: 'Joanne Kyger',
+    genre: 'Poetry',
+    year: '2017',
+    image: 'books/joanne.jpeg',
+  },
+  {
+    name: 'Fundamentals of Fluvial Geomorphology',
+    author: 'Ro Charlton',
+    genre: 'Science',
+    year: '2007',
+    image: 'books/fluvial.jpg',
+  },
+  {
+    name: 'Empathy',
+    author: 'Sarah Shulman',
+    genre: 'Fiction',
+    year: '1992',
+    image: 'books/empathy.jpg',
+  },
+
+];
