36827a9c7d4a569b9c6ccd9cdb69247648b627f6
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-27 14:39:01 -0700

    add dropdown menu and text input to BookSearch component

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -6,9 +6,15 @@ export default class BookSearch extends Component {
   render() {
     return (
       <form className="BookSearch">
-
+        <input/>
+        <select>
+          <option value="">Sort By...</option>
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="Genre">Genre</option>
+        </select>  
         <button>📚</button>
-
+        
       </form>
     );
   }
