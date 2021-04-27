8a9c096e301ef0ceb4035c149c9223c4fdc43923
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-27 14:55:10 -0700

    create handlers for BookSearch

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,12 +2,30 @@ import { Component } from 'react';
 import './BookSearch.css';
 
 export default class BookSearch extends Component {
+  state = {
+    nameSearch: '',
+    sortField: '',
+  }
+
+  handleNameChange = ({ target }) => {
+    this.setState({ nameSearch: target.value });
+  }
+
+  handleSearchChange = ({ target }) => {
+    this.setState({ sortField: target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSearch(this.state);
+  }
 
   render() {
+    const { nameSearch, sortField } = this.state;
     return (
-      <form className="BookSearch">
-        <input/>
-        <select>
+      <form className="BookSearch" onSubmit={this.handleSubmit}>
+        <input name="nameSearch" value={nameSearch} onChange={this.handleNameChange} />
+        <select name="sortField" value={sortField} onChange={this.handleSearchChange}>
           <option value="">Sort By...</option>
           <option value="title">Title</option>
           <option value="author">Author</option>
